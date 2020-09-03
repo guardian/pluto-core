a18f5cb88b2a83ce05b9c1abe316192cf0e57b2f
Viktor Ölund <viktor.olund@codemill.se>
2020-09-03 13:06:51 +0200

    create deliverable button and don't display error on 404

--- frontend/app/ProjectEntryList/ProjectEntryDeliverablesComponent.tsx
+++ frontend/app/ProjectEntryList/ProjectEntryDeliverablesComponent.tsx
@@ -11,8 +11,11 @@ import {
   TableBody,
   TableCell,
 } from "@material-ui/core";
-import { getProjectDeliverables } from "../utils/api";
+import { getProjectDeliverables, createProjectDeliverable } from "../utils/api";
 import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import SystemNotification, {
+  SystemNotificationKind,
+} from "../SystemNotification";
 
 const useStyles = makeStyles({
   projectDeliverable: {
@@ -42,6 +45,12 @@ const useStyles = makeStyles({
       marginTop: "1rem",
     },
   },
+  loading: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    alignItems: "center",
+  },
 });
 
 const tableHeaderTitles: string[] = ["Filename", "Size", "Status"];
@@ -55,6 +64,7 @@ const ProjectEntryDeliverablesComponent: React.FC<ProjectEntryDeliverablesCompon
 ) => {
   const classes = useStyles();
   const [deliverables, setDeliverables] = useState<Deliverable[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const [failed, setFailed] = useState<string>("");
   const { project } = props;
 
@@ -65,18 +75,46 @@ const ProjectEntryDeliverablesComponent: React.FC<ProjectEntryDeliverablesCompon
 
         setDeliverables(deliverables);
       } catch (error) {
-        let message = "Failed to fetch Deliverables!";
-        if (error?.response?.status === 503) {
-          message = `${message} - Reason: Pluto-Deliverables application is offline for some reason`;
-        }
+        if (error) {
+          let message = "Failed to fetch Deliverables!";
+          if (error?.response?.status === 503) {
+            message = `${message} - Reason: Pluto-Deliverables application is offline for some reason`;
+          }
 
-        setFailed(message);
-        console.error(message, error);
+          setFailed(message);
+          console.error(message, error);
+        }
       }
+
+      setLoading(false);
     };
 
+    setLoading(true);
     loadProjectDeliverables();
   }, []);
+
+  const createDeliverable = async (): Promise<void> => {
+    try {
+      await createProjectDeliverable(project);
+      window.location.assign("/deliverables/");
+    } catch (error) {
+      if (error) {
+        console.error(error);
+        SystemNotification.open(
+          SystemNotificationKind.Error,
+          "Failed to create Project Deliverable"
+        );
+      }
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className={classes.loading}>
+        <Typography variant="h4">Loading...</Typography>
+      </div>
+    );
+  }
 
   return (
     <Paper className={classes.projectDeliverable}>
@@ -90,9 +128,20 @@ const ProjectEntryDeliverablesComponent: React.FC<ProjectEntryDeliverablesCompon
           </div>
         </Typography>
       )}
-      <Typography variant="subtitle1">
-        Amount of Deliverables: <b>{deliverables?.length}</b>
-      </Typography>
+
+      {(!deliverables || deliverables.length === 0) && (
+        <>
+          <Typography variant="subtitle1">
+            No deliverables exists. Click "Create Deliverable" to create a
+            deliverable
+          </Typography>
+          <div className="button-container">
+            <Button variant="outlined" onClick={createDeliverable}>
+              Create Deliverable
+            </Button>
+          </div>
+        </>
+      )}
 
       {deliverables?.length > 0 && (
         <TableContainer>
@@ -116,12 +165,6 @@ const ProjectEntryDeliverablesComponent: React.FC<ProjectEntryDeliverablesCompon
           </Table>
         </TableContainer>
       )}
-
-      <div className="button-container">
-        <Button variant="outlined" href="/deliverables/">
-          Go to Deliverables
-        </Button>
-      </div>
     </Paper>
   );
 };
--- frontend/app/utils/api.ts
+++ frontend/app/utils/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const API = "/api";
 const API_IS_LOGGED_IN = `${API}/isLoggedIn`;
-const API_DELIVERABLES = `/deliverables${API}/deliverables`;
+const API_DELIVERABLES = `/deliverables${API}`;
 
 export const isLoggedIn = async (): Promise<PlutoUser> => {
   try {
@@ -23,17 +24,65 @@ export const getProjectDeliverables = async (
   projectId: number
 ): Promise<Deliverable[]> => {
   try {
-    const { status, data } = await axios.get<Deliverable[]>(
-      `${API_DELIVERABLES}?project_id=${projectId}`
+    const response = await axios.get<Deliverable[]>(
+      `${API_DELIVERABLES}/deliverables?project_id=${projectId}`
     );
 
-    if (status === 200) {
-      return data;
+    if (response?.status === 200) {
+      return response?.data;
     }
 
-    throw new Error(`Could not fetch Project deliverables. ${status}`);
+    if (response?.status === 404) {
+      throw "";
+    }
+
+    throw "Could not fetch Project deliverables";
   } catch (error) {
+    if (error?.response?.status === 404) {
+      return Promise.reject();
+    }
     console.error(error);
-    throw error;
+    return Promise.reject(error);
+  }
+};
+
+export const createProjectDeliverable = async (
+  project: Project
+): Promise<Deliverable> => {
+  const csrftoken = Cookies.get("csrftoken");
+  if (!csrftoken) {
+    console.warn("Could not find a csrf token! Request will probably fail");
+  }
+
+  try {
+    const response = await axios.post<Deliverable>(
+      `${API_DELIVERABLES}/bundle/new`,
+      {
+        pluto_core_project_id: project.id,
+        commission_id: project.commissionId,
+        name: project.title,
+      },
+      {
+        headers: {
+          "X-CSRFToken": csrftoken,
+        },
+      }
+    );
+
+    if (response?.status == 200) {
+      return response?.data;
+    }
+
+    if (response?.status === 404) {
+      throw "";
+    }
+
+    throw "Could not create Project deliverable";
+  } catch (error) {
+    if (error?.response?.status === 404) {
+      return Promise.reject();
+    }
+    console.error(error);
+    return Promise.reject(error);
   }
 };
