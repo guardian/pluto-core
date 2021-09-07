bc331e41e9ebcb8cc0317c034e6a2808752786de
Andy Gallagher <andy.gallagher@theguardian.com>
2021-09-07 18:29:30 +0100

    present found problems in a table

--- frontend/app/ProjectRecordValidation/ValidationJobResults.tsx
+++ frontend/app/ProjectRecordValidation/ValidationJobResults.tsx
@@ -3,13 +3,23 @@ import { useLocation, useParams } from "react-router-dom";
 import axios from "axios";
 import {
   Grid,
+  IconButton,
   LinearProgress,
   makeStyles,
   Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   Typography,
 } from "@material-ui/core";
 import { Alert } from "@material-ui/lab";
 import { differenceInMinutes, format, parseISO } from "date-fns";
+import ValidationTableRow from "./ValidationTableRow";
+import { useHistory } from "react-router";
+import { ArrowBackRounded } from "@material-ui/icons";
 
 interface ValidationJobResultsLocationParams {
   jobId: string;
@@ -19,6 +29,13 @@ const useStyles = makeStyles({
   headerTitle: {
     fontWeight: "bold",
     fontSize: "1.1rem",
+  },
+  resultsTable: {
+    maxHeight: "60vh",
+  },
+  infoBanner: {
+    marginTop: "1em",
+    marginBottom: "1em",
   },
 });
 
@@ -32,6 +49,7 @@ const ValidationJobResults: React.FC = () => {
   const [lastError, setLastError] = useState<string | undefined>(undefined);
 
   const routerParams = useParams<ValidationJobResultsLocationParams>();
+  const history = useHistory();
 
   const classes = useStyles();
 
@@ -137,7 +155,12 @@ const ValidationJobResults: React.FC = () => {
 
       <Paper elevation={3}>
         {jobDetails ? (
-          <Grid container spacing={5} style={{ margin: "1em" }}>
+          <Grid container spacing={5} className={classes.infoBanner}>
+            <Grid item>
+              <IconButton onClick={() => history.push("/validate/project")}>
+                <ArrowBackRounded />
+              </IconButton>
+            </Grid>
             <Grid item>
               <Typography className={classes.headerTitle}>Job ID</Typography>
               <Typography>{jobDetails.uuid}</Typography>
@@ -176,6 +199,32 @@ const ValidationJobResults: React.FC = () => {
             </Grid>
           </Grid>
         ) : undefined}
+      </Paper>
+
+      <Paper elevation={3}>
+        <TableContainer>
+          <Table size="small" className={classes.resultsTable}>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography>Problem detected at</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>Affected item</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>Details</Typography>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {problemReports.map((entry, idx) => (
+                <ValidationTableRow data={entry} key={idx} />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Paper>
     </>
   );
