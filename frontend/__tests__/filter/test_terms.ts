f35f575fa0a161aff546d5b55526f6fa7fef2da0
Dave Allison <david.allison@theguardian.com>
2021-11-12 15:45:38 +0000

    New tests

---
+++ frontend/__tests__/filter/test_terms.ts
@@ -0,0 +1,93 @@
+import { buildFilterTerms } from "../../app/filter/terms";
+
+describe("terms.buildFilterTerms", () => {
+  it("return the correct value when showKilled is set to true in the URL", () => {
+    global.window = Object.create(window);
+    const url = "?mine&title=test&user=mr_flibble&group=1&showKilled=true";
+    Object.defineProperty(window, "location", {
+      value: {
+        search: url,
+      },
+      writable: true,
+    });
+    const objectOutput = buildFilterTerms(
+      "?mine&title=test&user=mr_flibble&group=1&showKilled=true"
+    );
+    expect(objectOutput.showKilled).toBe(true);
+  });
+
+  it("return the correct value when showKilled is set to false in the URL", () => {
+    global.window = Object.create(window);
+    const url2 = "?mine&title=test&user=mr_flibble&group=1&showKilled=false";
+    Object.defineProperty(window, "location", {
+      value: {
+        search: url2,
+      },
+    });
+    const objectOutput2 = buildFilterTerms(
+      "?mine&title=test&user=mr_flibble&group=1&showKilled=false"
+    );
+    expect(objectOutput2.showKilled).toBe(false);
+  });
+
+  it("return the correct value when title is set to 'test' in the URL", () => {
+    global.window = Object.create(window);
+    const url3 = "?mine&title=test&user=mr_flibble&group=1&showKilled=false";
+    Object.defineProperty(window, "location", {
+      value: {
+        search: url3,
+      },
+    });
+    const objectOutput3 = buildFilterTerms(
+      "?mine&title=test&user=mr_flibble&group=1&showKilled=false"
+    );
+    expect(objectOutput3.title).toBe("test");
+  });
+
+  it("return the correct value when group is set to '1' in the URL", () => {
+    global.window = Object.create(window);
+    const url4 = "?mine&title=test&user=mr_flibble&group=1&showKilled=false";
+    Object.defineProperty(window, "location", {
+      value: {
+        search: url4,
+      },
+    });
+    const objectOutput4 = buildFilterTerms(
+      "?mine&title=test&user=mr_flibble&group=1&showKilled=false"
+    );
+    expect(objectOutput4.group).toBe("1");
+  });
+
+  const testUser = <PlutoUser>{
+    uid: "1",
+  };
+
+  it("return the correct value of match when a user object is present and 'mine' is in the URL", () => {
+    global.window = Object.create(window);
+    const url5 = "?mine&title=test&user=mr_flibble&group=1&showKilled=false";
+    Object.defineProperty(window, "location", {
+      value: {
+        search: url5,
+      },
+    });
+    const objectOutput5 = buildFilterTerms(
+      "?mine&title=test&user=mr_flibble&group=1&showKilled=false",
+      testUser
+    );
+    expect(objectOutput5.match).toBe("W_EXACT");
+  });
+
+  it("return the correct value of match when no user object is present and 'mine' is not in the URL", () => {
+    global.window = Object.create(window);
+    const url6 = "?title=test&user=mr_flibble&group=1&showKilled=false";
+    Object.defineProperty(window, "location", {
+      value: {
+        search: url6,
+      },
+    });
+    const objectOutput6 = buildFilterTerms(
+      "?mine&title=test&user=mr_flibble&group=1&showKilled=false"
+    );
+    expect(objectOutput6.match).toBe("W_CONTAINS");
+  });
+});
