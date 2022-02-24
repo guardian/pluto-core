25a79384c602c77ff5799f2e33080e7f32f5bfa3
Dave Allison <david.allison@theguardian.com>
2022-02-24 11:40:48 +0000

    Test that checks if permissions are set correctly on storage for files with a cpr extension

--- test/postrun/CopyProjectToAssetFolderSpec.scala
+++ test/postrun/CopyProjectToAssetFolderSpec.scala
@@ -5,10 +5,14 @@ import models.{ProjectEntry, ProjectType}
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
-import java.nio.file.{Path, Paths}
+import java.nio.file.attribute.{PosixFileAttributeView, PosixFilePermission, PosixFilePermissions}
+import java.nio.file.{Files, Path, Paths}
 import scala.concurrent.Await
 import scala.util.{Success, Try}
 import scala.concurrent.duration._
+import scala.jdk.CollectionConverters._
+import org.apache.commons.io.FileUtils
+import java.io.File
 
 class CopyProjectToAssetFolderSpec extends Specification with Mockito {
   "CopyProjectToAssetFolder" should {
@@ -60,6 +64,61 @@ class CopyProjectToAssetFolderSpec extends Specification with Mockito {
         any,
         any
       )
+    }
+
+    "check permissions are set correctly if the file has a cpr extension" in {
+      FileUtils.deleteQuietly(new File("/tmp/test.cpr"))
+      val mockCopy = mock[(Path,Path)=>Try[Unit]]
+      mockCopy.apply(any, any) returns Success( () )
+      val mockSetPerms = mock[(Path,Path)=>Try[Unit]]
+      mockSetPerms.apply(any, any) returns Success( () )
+      val mockDataCache = mock[PostrunDataCache]
+      mockDataCache.get("created_asset_folder") returns Some("/tmp")
+
+      val toTest = new CopyProjectToAssetfolder {
+        override protected def preservePermissionsAndOwnership(from: Path, to: Path): Try[Unit] = mockSetPerms(from, to)
+      }
+
+      val result = Await.result(
+        toTest.postrun("postrun/tests/data/test.cpr",mock[ProjectEntry],mock[ProjectType], mockDataCache, None,None),
+        1.second)
+
+      def attributeViewFor(path:Path) = Files.getFileAttributeView(path, classOf[PosixFileAttributeView])
+
+      val destView = attributeViewFor(Paths.get("/tmp/test.cpr").toAbsolutePath)
+
+      val targetPerms:java.util.Set[PosixFilePermission] = Array(
+          PosixFilePermission.GROUP_READ,
+          PosixFilePermission.GROUP_WRITE,
+          PosixFilePermission.OTHERS_READ,
+          PosixFilePermission.OWNER_READ,
+          PosixFilePermission.OWNER_WRITE
+        ).toSet.asJava
+
+      destView.setPermissions(targetPerms)
+
+      val toTestTwo = new CopyProjectToAssetfolder {
+        override protected def doCopyFile(from: Path, to: Path): Try[Unit] = mockCopy(from, to)
+      }
+
+      val resultTwo = Await.result(
+        toTestTwo.postrun("postrun/tests/data/test.cpr",mock[ProjectEntry],mock[ProjectType], mockDataCache, None,None),
+        1.second)
+
+      val destViewTwo = attributeViewFor(Paths.get("/tmp/test.cpr").toAbsolutePath)
+
+      val targetPermsTwo:java.util.Set[PosixFilePermission] = Array(
+        PosixFilePermission.GROUP_READ,
+        PosixFilePermission.GROUP_WRITE,
+        PosixFilePermission.OTHERS_READ,
+        PosixFilePermission.OTHERS_WRITE,
+        PosixFilePermission.OWNER_READ,
+        PosixFilePermission.OWNER_WRITE
+      ).toSet.asJava
+
+      destViewTwo.readAttributes().permissions() mustEqual targetPermsTwo
+
+      FileUtils.deleteQuietly(new File("/tmp/test.cpr"))
     }
   }
 }
