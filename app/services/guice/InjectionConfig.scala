0b7c0ce7f4443da937f92eaf8e2937d61d8446a1
Dave Allison <david.allison@theguardian.com>
2022-12-14 11:44:05 +0000

    Bind a new actor

--- app/services/guice/InjectionConfig.scala
+++ app/services/guice/InjectionConfig.scala
@@ -24,6 +24,7 @@ class InjectionConfig extends AbstractModule with AkkaGuiceSupport {
     bindActor[RabbitMqSend]("rabbitmq-send")
     bindActor[RabbitMqDeliverable]("rabbitmq-deliverable")
     bindActor[RabbitMqSAN]("rabbitmq-san")
+    bindActor[RabbitMqMatrix]("rabbitmq-matrix")
     bindActor[Auditor]("auditor")
     bind(classOf[PeriodicScanReceiver]).asEagerSingleton()
     bind(classOf[MXSConnectionManager]).asEagerSingleton()
