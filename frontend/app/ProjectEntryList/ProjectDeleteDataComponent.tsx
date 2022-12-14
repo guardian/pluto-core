1964e92a825256cb1d1d6de345bb46eefe4b2623
Dave Allison <david.allison@theguardian.com>
2022-12-14 17:25:07 +0000

    Only show text about not deleted items when needed

--- frontend/app/ProjectEntryList/ProjectDeleteDataComponent.tsx
+++ frontend/app/ProjectEntryList/ProjectDeleteDataComponent.tsx
@@ -343,7 +343,7 @@ const ProjectDeleteDataComponent: React.FC<ProjectDeleteDataComponentProps> = (
                   {deleteJobStatus == "Finished" ? (
                     <>
                       Job finished
-                      {itemsNotDeleted ? (
+                      {itemsNotDeleted.length > 0 ? (
                         <>
                           <br />
                           <br />
