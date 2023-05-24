3f3bf0e766e3779de74e9f86cf993f350416a9fe
Andy Gallagher <fredex42@gmail.com>
2023-05-24 09:55:55 +0100

    removes the ldap check from the healthcheck endpoint.  This is a temporary measure for the phase1 system. (#14)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -221,11 +221,11 @@ class Application @Inject() (val cc:ControllerComponents,
   }
 
   def healthcheck = Action.async { request=>
-    val checkFutures = Future.sequence(Seq(LDAP.hasConnectionPool,dbHelper.healthcheck))
+    val checkFutures = Future.sequence(Seq(dbHelper.healthcheck))
 
     checkFutures.map(resultSeq=>{
-      val ldapCheck = resultSeq.head
-      val dbCheck = resultSeq(1)
+      val ldapCheck = Success( () ) //dont b0rk on a failing ldap check
+      val dbCheck = resultSeq.head
       val ldapMode = config.getOptional[String]("ldap.ldapProtocol").getOrElse("ldap")
 
       if( (ldapMode!="none" && ldapCheck.isFailure) || dbCheck.isFailure){
