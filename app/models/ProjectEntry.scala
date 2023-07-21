fecb9f55acfaf8e68200ca1efa57c1f4ed7cf596
Richard Pryce <richard.pryce@guardian.co.uk>
2023-07-21 13:48:03 +0100

    Refactor dbActionForStatusUpdate

--- app/models/ProjectEntry.scala
+++ app/models/ProjectEntry.scala
@@ -234,37 +234,34 @@ object ProjectEntry extends ((Option[Int], Int, Option[String], String, Timestam
   def dbActionForStatusUpdate(newStatus: EntryStatus.Value, commissionId: Int): DBIO[Seq[(Int, ProjectEntry)]] = {
     import EntryStatusMapper._
 
+    def updateProjects(query: Query[ProjectEntryRow, ProjectEntry, Seq]) = {
+      query.result.flatMap { projects =>
+        val ids = projects.map(_.id.getOrElse(-1))
+        val updateActions = projects.map(p => query.filter(_.id === p.id).map(_.status).update(newStatus).map(_ => p))
+        DBIO.sequence(updateActions).map(updatedProjects => ids.zip(updatedProjects))
+      }
+    }
+
+    val baseQuery = TableQuery[ProjectEntryRow].filter(_.commission === commissionId)
+
     newStatus match {
       case EntryStatus.Completed | EntryStatus.Killed =>
-        val query = TableQuery[ProjectEntryRow]
-          .filter(_.commission === commissionId)
+        val filteredQuery = baseQuery
           .filter(_.status =!= EntryStatus.Completed)
           .filter(_.status =!= EntryStatus.Killed)
-
-        query.result.flatMap { projects =>
-          val ids = projects.map(_.id.getOrElse(-1))
-          val updateActions = projects.map(p => query.filter(_.id === p.id).map(_.status).update(newStatus).map(_ => p))
-
-          DBIO.sequence(updateActions).map(updatedProjects => ids.zip(updatedProjects))
-        }
+        updateProjects(filteredQuery)
 
       case EntryStatus.Held =>
-        val query = TableQuery[ProjectEntryRow]
-          .filter(_.commission === commissionId)
+        val filteredQuery = baseQuery
           .filter(_.status =!= EntryStatus.Completed)
           .filter(_.status =!= EntryStatus.Killed)
           .filter(_.status =!= EntryStatus.Held)
-
-        query.result.flatMap { projects =>
-          val ids = projects.map(_.id.getOrElse(-1))
-          val updateActions = projects.map(p => query.filter(_.id === p.id).map(_.status).update(newStatus).map(_ => p))
-
-          DBIO.sequence(updateActions).map(updatedProjects => ids.zip(updatedProjects))
-        }
+        updateProjects(filteredQuery)
 
       case _ => DBIO.successful(Seq.empty[(Int, ProjectEntry)])
     }
   }
+
 
   def createFromFile(sourceFile: FileEntry, projectTemplate: ProjectTemplate, title:String, created:Option[LocalDateTime],
                      user:String, workingGroupId: Option[Int], commissionId: Option[Int], existingVidispineId: Option[String],
