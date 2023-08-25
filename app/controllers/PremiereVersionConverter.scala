b7f772c4a6ffb71ede1a2b37271e89a8ba79c214
Richard Pryce <richard.pryce@guardian.co.uk>
2023-08-25 14:37:48 +0100

    Only change version on major upgrade

--- app/controllers/PremiereVersionConverter.scala
+++ app/controllers/PremiereVersionConverter.scala
@@ -39,7 +39,7 @@ class PremiereVersionConverter @Inject()(override val controllerComponents:Contr
       case Some(newVersion)=>
         val resultFut = for {
           targetVersion <- premiereVersionTranslationDAO
-            .findDisplayedVersion(newVersion)
+            .findDisplayedVersionByMajor(newVersion)
             .flatMap(results=>{
               if(results.isEmpty) {
                 Future.failed(new RuntimeException("Version number is not recognised"))
