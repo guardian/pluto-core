952397299134eb34582641adbaee6ea09f5fb012
Richard Pryce <richard.pryce@guardian.co.uk>
2023-09-05 10:13:55 +0100

    Add tooltip information to checkboxes

--- frontend/app/ProjectEntryList/ApplicableRulesSelector.tsx
+++ frontend/app/ProjectEntryList/ApplicableRulesSelector.tsx
@@ -3,6 +3,7 @@ import {
   Checkbox,
   FormControlLabel,
   Grid,
+  Tooltip,
   Typography,
 } from "@material-ui/core";
 
@@ -19,39 +20,47 @@ const ApplicableRulesSelector: React.FC<ApplicableRulesSelectorProps> = (
   return (
     <>
       <Typography>Applicable rules</Typography>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={props.deletable}
-            onChange={() => props.onChange("deletable", props.deletable)}
-            name="deletable"
-            color="primary"
-          />
-        }
-        label="Deletable"
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={props.deep_archive}
-            onChange={() => props.onChange("deep_archive", props.deep_archive)}
-            name="deep_archive"
-            color="primary"
-          />
-        }
-        label="Deep Archive"
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={props.sensitive}
-            onChange={() => props.onChange("sensitive", props.sensitive)}
-            name="sensitive"
-            color="primary"
-          />
-        }
-        label="Sensitive"
-      />
+      <Tooltip title="Selecting this option means that the data will not be backed up. Once the project is set to 'Completed', it will be permanently deleted.">
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={props.deletable}
+              onChange={() => props.onChange("deletable", props.deletable)}
+              name="deletable"
+              color="primary"
+            />
+          }
+          label="Deletable"
+        />
+      </Tooltip>
+      <Tooltip title="This option will backup the data to long-term storage">
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={props.deep_archive}
+              onChange={() =>
+                props.onChange("deep_archive", props.deep_archive)
+              }
+              name="deep_archive"
+              color="primary"
+            />
+          }
+          label="Deep Archive"
+        />
+      </Tooltip>
+      <Tooltip title="Data marked as 'Sensitive' will not be stored in the cloud but will instead be stored on-premises.">
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={props.sensitive}
+              onChange={() => props.onChange("sensitive", props.sensitive)}
+              name="sensitive"
+              color="primary"
+            />
+          }
+          label="Sensitive"
+        />
+      </Tooltip>
     </>
   );
 };
