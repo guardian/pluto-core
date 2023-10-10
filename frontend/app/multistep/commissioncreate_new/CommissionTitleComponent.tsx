6fe22792921b344db2e9e38ef0feaa4ff93e8b06
Richard Pryce <richard.pryce@guardian.co.uk>
2023-10-10 10:11:35 +0100

    Reduce number of create commission steps

--- frontend/app/multistep/commissioncreate_new/CommissionTitleComponent.tsx
+++ frontend/app/multistep/commissioncreate_new/CommissionTitleComponent.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Grid, Input, TextField, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Grid, Input, Paper, TextField, Typography } from "@material-ui/core";
 import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
@@ -8,6 +8,9 @@ import {
 } from "@material-ui/pickers";
 import { useGuardianStyles } from "~/misc/utils";
 import ProductionOfficeComponent from "../projectcreate_new/ProductionOfficeComponent";
+import WorkingGroupSelector from "../common/WorkingGroupSelector";
+import { loadWorkingGroups } from "../common/WorkingGroupService";
+import { Autocomplete } from "@material-ui/lab";
 
 interface CommissionTitleComponentProps {
   title: string;
@@ -16,98 +19,94 @@ interface CommissionTitleComponentProps {
   onExpirationChanged: (newValue: Date) => void;
   valueWasSet: (newValue: ProductionOffice) => void;
   productionOfficeValue: string;
+  workingGroupId?: number;
+  workingGroupIdDidChange: (newValue: number) => void;
 }
 
 const CommissionTitleComponent: React.FC<CommissionTitleComponentProps> = (
   props
 ) => {
   const classes = useGuardianStyles();
+  const [knownWorkingGroups, setKnownWorkingGroups] = useState<WorkingGroup[]>(
+    []
+  );
+  useEffect(() => {
+    loadWorkingGroups(setKnownWorkingGroups).catch((err) => {
+      console.error("Could not load working groups: ", err);
+    });
+  }, []);
 
   return (
     <div className={classes.common_box_size}>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Typography variant="h3">Commission Configuration</Typography>
-        {/* <Typography>
-          Now, we need a descriptive name for your new commission and we need to
-          know when it is scheduled to be completed.
-        </Typography>
-        <Typography>
-          This value can be updated at any time in the future, so don't worry
-          too much about setting something very long now.
-          <br />
-          Beware, if you set a value too far in the future, it is possible that
-          at some point in the future MM Tech will conclude that your commission
-          was actually finished long ago and archive it; so please try to be
-          accurate.
-        </Typography> */}
         <Grid container direction="column">
-          <Grid item xs={6} sm={6}>
-            <TextField
-              style={{ width: "100%" }}
-              label="Commission title"
-              placeholder="Commission title"
-              helperText="Enter a good descriptive Commission titlee"
-              margin="normal"
-              id="commissionNameInput"
-              onChange={(event) => props.onTitleChanged(event.target.value)}
-              value={props.title}
-            />
-          </Grid>
-          <Grid item xs={6} sm={6}>
-            <KeyboardDatePicker
-              className={classes.inputBox}
-              disableToolbar
-              variant="inline"
-              format="dd/MM/yyyy"
-              margin="normal"
-              label="Scheduled completion"
-              helperText="This value can be updated at any time in the future, so don't worry
-                  too much about setting something very long now."
-              value={props.expiration}
-              onChange={(newValue) =>
-                newValue ? props.onExpirationChanged(newValue) : undefined
-              }
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <ProductionOfficeComponent
-              valueWasSet={props.valueWasSet}
-              productionOfficeValue={props.productionOfficeValue}
-            />
-          </Grid>
-        </Grid>
-        {/* <table>
-          <tbody>
-            <tr>
-              <td>Commission name</td>
-              <td>
-                <Input
-                  className={classes.inputBox}
-                  id="commissionNameInput"
-                  onChange={(evt) => props.onTitleChanged(evt.target.value)}
-                  value={props.title}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>Scheduled Completion</td>
-              <td>
-                <KeyboardDatePicker
-                  className={classes.inputBox}
-                  disableToolbar
-                  variant="inline"
-                  format="dd/MM/yyyy"
-                  margin="normal"
-                  label="Scheduled completion"
-                  value={props.expiration}
-                  onChange={(newValue) =>
-                    newValue ? props.onExpirationChanged(newValue) : undefined
+          <Paper className={classes.paperWithPadding}>
+            <Grid item xs={6} sm={6}>
+              <TextField
+                style={{ width: 300 }}
+                label="Commission Title"
+                placeholder="My commission title"
+                helperText="Enter a good descriptive Commission title"
+                margin="normal"
+                id="commissionNameInput"
+                onChange={(event) => props.onTitleChanged(event.target.value)}
+                value={props.title}
+              />
+            </Grid>
+          </Paper>
+          <Paper className={classes.paperWithPadding}>
+            <Grid item xs={6} sm={6}>
+              <KeyboardDatePicker
+                className={classes.inputBox}
+                style={{ width: 300 }}
+                disableToolbar
+                variant="inline"
+                format="dd/MM/yyyy"
+                margin="normal"
+                label="Scheduled completion"
+                helperText="This value can be updated at any time in the future."
+                value={props.expiration}
+                onChange={(newValue) =>
+                  newValue ? props.onExpirationChanged(newValue) : undefined
+                }
+              />
+            </Grid>
+          </Paper>
+          <Paper className={classes.paperWithPadding}>
+            <Grid item xs={12}>
+              <ProductionOfficeComponent
+                valueWasSet={props.valueWasSet}
+                productionOfficeValue={props.productionOfficeValue}
+              />
+            </Grid>
+          </Paper>
+          <Paper className={classes.paperWithPadding}>
+            <Grid item>
+              <Autocomplete
+                options={knownWorkingGroups}
+                getOptionLabel={(option) => option.name}
+                renderInput={(params) => (
+                  <TextField
+                    style={{ width: 300 }}
+                    {...params}
+                    label="Working Group"
+                  />
+                )}
+                onChange={(event, newValue) => {
+                  if (newValue) {
+                    props.workingGroupIdDidChange(newValue.id);
                   }
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table> */}
+                }}
+                PaperComponent={({ children, ...other }) => (
+                  <Paper {...other} style={{ maxHeight: 370 }}>
+                    {children}
+                  </Paper>
+                )}
+              />
+            </Grid>
+          </Paper>
+        </Grid>
       </MuiPickersUtilsProvider>
     </div>
   );
