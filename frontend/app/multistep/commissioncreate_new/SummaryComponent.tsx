ac76895be0dab198f007d2bfec90e838a78c2553
Richard Pryce <richard.pryce@guardian.co.uk>
2023-10-13 14:50:32 +0100

    Refactor commission summary page

--- frontend/app/multistep/commissioncreate_new/SummaryComponent.tsx
+++ frontend/app/multistep/commissioncreate_new/SummaryComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Typography } from "@material-ui/core";
+import { Divider, Grid, Paper, Typography } from "@material-ui/core";
 import format from "date-fns/format";
 import { isBefore, isAfter, addYears } from "date-fns";
 import WorkingGroupEntryView from "../../EntryViews/WorkingGroupEntryView";
@@ -17,76 +17,95 @@ const SummaryComponent: React.FC<SummaryComponentProps> = (props) => {
   const classes = useGuardianStyles();
 
   const userContext = useContext(UserContext);
-  return (
-    <div className={classes.common_box_size}>
-      <Typography variant="h2">Create a new commission</Typography>
-      <Typography>
-        We will create a new commission with the information below.
-        <br />
-        Press "Create" to go ahead, or Back if you need to amend any details.
-      </Typography>
+  const italicTextStyle = {
+    fontStyle: "italic",
+  };
 
-      <table>
-        <tbody>
-          <tr>
-            <td>Working group</td>
-            <td id="wg-value">
-              {props.workingGroupId ? (
-                <WorkingGroupEntryView entryId={props.workingGroupId} />
-              ) : (
-                "not set"
-              )}
-            </td>
-            <td id="wg-error">
-              {props.workingGroupId ? undefined : (
-                <Typography className={classes.error}>
-                  You must set a working group
-                </Typography>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>Commission name</td>
-            <td id="title-value">{props.title}</td>
-            <td id="title-error">
-              {props.title === "" ? (
-                <Typography className={classes.error}>
-                  You can't create a commission without a name
-                </Typography>
-              ) : undefined}
-            </td>
-          </tr>
-          <tr>
-            <td>Scheduled completion</td>
-            <td id="scheduled-completion-value">
-              {format(props.scheduledCompetion, "iiii, do MMM yyyy")}
-            </td>
-            <td id="scheduled-completion-err">
-              {isBefore(props.scheduledCompetion, new Date()) ? (
-                <Typography className={classes.error}>
-                  You can't create a commission with a completion date in the
-                  past
-                </Typography>
-              ) : undefined}
-              {isAfter(props.scheduledCompetion, addYears(new Date(), 1)) ? (
-                <Typography className={classes.error}>
-                  This is a long way in the future, you should change it to
-                  something more realistic
-                </Typography>
-              ) : undefined}
-            </td>
-          </tr>
-          <tr>
-            <td>Production office</td>
-            <td id="productionoffice-value">{props.productionOffice}</td>
-          </tr>
-          <tr>
-            <td>Owner</td>
-            <td id="owner-value">{userContext?.userName ?? "not found"}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+  return (
+    <Paper
+      className={classes.common_box_size}
+      style={{
+        padding: "40px",
+        overflowY: "auto",
+        maxHeight: "90vh",
+      }}
+    >
+      <Typography variant="h3" gutterBottom>
+        New commission review
+      </Typography>
+      <Typography style={{ fontSize: "0.75rem", marginBottom: "20px" }}>
+        A new commission will be created with the information below.
+        <br />
+        Press "Create" to proceed, or "Back" if you need to amend any details.
+      </Typography>
+      <Divider />
+
+      <Grid container spacing={3} style={{ marginTop: "20px" }}>
+        <Grid item xs={12} sm={4}>
+          Working group
+        </Grid>
+        <Grid item xs={12} sm={6} style={italicTextStyle}>
+          {props.workingGroupId ? (
+            props.workingGroupId
+          ) : (
+            <span style={{ color: "red" }}>Not provided</span>
+          )}
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          Commission name
+        </Grid>
+        <Grid item xs={12} sm={6} style={italicTextStyle}>
+          {props.title ? (
+            props.title
+          ) : (
+            <span style={{ color: "red" }}>Not provided</span>
+          )}
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          Scheduled completion
+        </Grid>
+        <Grid item xs={12} sm={6} style={italicTextStyle}>
+          {props.scheduledCompetion &&
+          !isBefore(props.scheduledCompetion, new Date()) &&
+          !isAfter(props.scheduledCompetion, addYears(new Date(), 1)) ? (
+            format(props.scheduledCompetion, "iiii, do MMM yyyy")
+          ) : !props.scheduledCompetion ? (
+            <span style={{ color: "red" }}>Not provided</span>
+          ) : isBefore(props.scheduledCompetion, new Date()) ? (
+            <span style={{ color: "red" }}>
+              You can't create a commission with a completion date in the past
+            </span>
+          ) : isAfter(props.scheduledCompetion, addYears(new Date(), 1)) ? (
+            <span style={{ color: "red" }}>
+              This is a long way in the future, you should change it to
+              something more realistic
+            </span>
+          ) : (
+            <span style={{ color: "red" }}>Something went wrong</span>
+          )}
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          Production office
+        </Grid>
+        <Grid item xs={12} sm={6} style={italicTextStyle}>
+          {props.productionOffice ? (
+            props.productionOffice
+          ) : (
+            <span style={{ color: "red" }}>Not provided</span>
+          )}
+        </Grid>
+
+        <Grid item xs={12} sm={4}>
+          Owner
+        </Grid>
+        <Grid item xs={12} sm={6} style={italicTextStyle}>
+          {userContext?.userName ?? "not found"}
+        </Grid>
+      </Grid>
+    </Paper>
   );
 };
 
