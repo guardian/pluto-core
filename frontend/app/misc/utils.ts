2070d121fda067f3dd9f46bd7ddae88d2d214a53
Richard Pryce <richard.pryce@guardian.co.uk>
2023-10-23 08:02:27 +0100

    Add hover row css

--- frontend/app/misc/utils.ts
+++ frontend/app/misc/utils.ts
@@ -432,4 +432,9 @@ export const useGuardianStyles = makeStyles((theme) => ({
       backgroundColor: "transparent",
     },
   },
+  hoverRow: {
+    "&:hover": {
+      backgroundColor: theme.palette.action.hover, // You can replace this with any color you prefer
+    },
+  },
 }));
