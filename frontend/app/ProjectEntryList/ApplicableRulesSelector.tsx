e2094a1932ace88e8feb2a32bc9ab8b48d2b5268
Richard Pryce <richard.pryce@guardian.co.uk>
2023-11-27 17:01:31 +0000

    Use radio buttons for media management options

--- frontend/app/ProjectEntryList/ApplicableRulesSelector.tsx
+++ frontend/app/ProjectEntryList/ApplicableRulesSelector.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-  Checkbox,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
   FormControlLabel,
-  Grid,
+  Radio,
+  RadioGroup,
   Tooltip,
   Typography,
 } from "@material-ui/core";
@@ -16,8 +22,17 @@ interface ApplicableRulesSelectorProps {
   disabled: boolean;
 }
 
+interface PendingChange {
+  field: keyof Project;
+  prevValue: boolean;
+}
+
 const useStyles = makeStyles((theme) => ({
   disabledText: {
+    color: "grey",
+  },
+  // Add styling for disabled radio button
+  disabledRadio: {
     color: "grey",
   },
 }));
@@ -26,56 +41,116 @@ const ApplicableRulesSelector: React.FC<ApplicableRulesSelectorProps> = (
   props
 ) => {
   const classes = useStyles();
+  const [openDialog, setOpenDialog] = useState(false);
+  const [pendingChange, setPendingChange] = useState<PendingChange | null>(
+    null
+  );
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    if (value in props) {
+      const key = value as keyof ApplicableRulesSelectorProps;
+      console.log("Key: ", key);
+      if (key === "deletable" && props.deep_archive) {
+        setPendingChange({ field: key, prevValue: props[key] });
+        setOpenDialog(true);
+      } else if (key === "deep_archive" && props.deletable) {
+        setPendingChange({ field: key, prevValue: props[key] });
+        setSelectedOption(value);
+        const projectKey = key as keyof Project;
+        props.onChange(projectKey, props[key] as boolean);
+      }
+    }
+  };
+
+  const handleDialogClose = (confirm: boolean) => {
+    setOpenDialog(false);
+    if (confirm && pendingChange) {
+      setSelectedOption("deletable");
+      props.onChange("deletable", false);
+      props.onChange("deep_archive", true);
+    } else if (pendingChange) {
+      console.log("Else: (pendingChange)", pendingChange);
+      setSelectedOption("deep_archive");
+      props.onChange("deletable", true);
+      props.onChange("deep_archive", false);
+    }
+    setPendingChange(null);
+  };
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  useEffect(() => {
+    let option = "";
+    if (props.deep_archive) {
+      option = "deep_archive";
+    } else if (props.deletable) {
+      option = "deletable";
+    }
+    setSelectedOption(option);
+  }, [props.deletable, props.deep_archive]);
 
   return (
     <>
       <Typography className={props.disabled ? classes.disabledText : ""}>
         Media management settings
       </Typography>
-      <Tooltip title="Data will be backed up to long-term storage">
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={props.deep_archive}
-              onChange={() =>
-                props.onChange("deep_archive", props.deep_archive)
-              }
-              name="deep_archive"
-              color="primary"
-              disabled={props.disabled}
-            />
-          }
-          label="Deep Archive"
-        />
-      </Tooltip>
-      <Tooltip title="Data will not be backed up. Once the project is set to 'Completed', it will be permanently deleted.">
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={props.deletable}
-              onChange={() => props.onChange("deletable", props.deletable)}
-              name="deletable"
-              color="primary"
-              disabled={props.disabled}
-            />
-          }
-          label="Deletable"
-        />
-      </Tooltip>
+      <RadioGroup value={selectedOption} onChange={handleChange}>
+        <Tooltip title="Data will be backed up to long-term storage">
+          <FormControlLabel
+            value="deep_archive"
+            control={<Radio color="primary" disabled={props.disabled} />}
+            label="Deep Archive"
+          />
+        </Tooltip>
+        <Tooltip title="Data will not be backed up. Once the project is set to 'Completed', it will be permanently deleted.">
+          <FormControlLabel
+            value="deletable"
+            control={<Radio color="primary" disabled={props.disabled} />}
+            label="Deletable"
+          />
+        </Tooltip>
+      </RadioGroup>
       <Tooltip title="Data marked as 'Sensitive' will not be stored in the cloud but will instead be stored on-premises.">
         <FormControlLabel
           control={
-            <Checkbox
+            <Radio
               checked={props.sensitive}
-              onChange={() => props.onChange("sensitive", props.sensitive)}
+              className={props.disabled ? classes.disabledRadio : ""}
               name="sensitive"
               color="primary"
-              disabled={props.disabled}
+              disabled
             />
           }
           label="Sensitive"
         />
       </Tooltip>
+
+      {/* Confirmation Dialog */}
+      <Dialog open={openDialog} onClose={() => handleDialogClose(false)}>
+        <DialogTitle>{"Are you sure?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Changing to "Deletable" means the project media will be permanently
+            deleted after it is set to Completed and its completion date has
+            passed.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleDialogClose(false)} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={() => handleDialogClose(true)}
+            color="primary"
+            autoFocus
+          >
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
