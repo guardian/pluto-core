a415ac154311e2e562fd16291ed779096aa45a14
Dave Allison <david.allison@theguardian.com>
2024-01-24 12:39:49 +0000

    Adding deletion of audio backups

--- app/services/BackupLauncher.scala
+++ app/services/BackupLauncher.scala
@@ -12,12 +12,13 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object BackupLauncher {
   private val logger = LoggerFactory.getLogger(getClass)
 
-  case class Options(nukeInvalidBackups:Boolean, backupAll:Boolean)
+  case class Options(nukeInvalidBackups:Boolean, backupAll:Boolean, deleteAudioBackups:Boolean)
   val parser = new OptionParser[Options]("backup-launcher") {
     head("backup-launcher", "1")
 
     opt[Boolean]("nuke-invalid") action { (x,c)=>c.copy(nukeInvalidBackups = x)} text("instead of launching a backup, remove zero-length backup files")
     opt[Boolean]("all") action { (x,c)=>c.copy(backupAll = x)} text "try to back up every project instead of just 'in production'"
+    opt[Boolean]("delete-audio") action { (x,c)=>c.copy(deleteAudioBackups = x)} text("Instead of launching a backup, remove audio backup files")
   }
 
   def main(args:Array[String]):Unit = {
@@ -28,13 +29,23 @@ object BackupLauncher {
     implicit val injector = app.injector
     val projectBackup = injector.instanceOf(classOf[NewProjectBackup])
 
-    parser.parse(args, Options(false, false)) match {
+    parser.parse(args, Options(false, false, false)) match {
       case Some(opts) =>
 
         if (opts.nukeInvalidBackups) {
           projectBackup.nukeInvalidBackups.onComplete({
             case Success(r) =>
               println(s"Nuked ${r.successCount} dodgy backups")
+              System.exit(0)
+            case Failure(exception) =>
+              println(s"ERROR - Could not complete scan: ${exception.getMessage}")
+              exception.printStackTrace()
+              System.exit(1)
+          })
+        } else if (opts.deleteAudioBackups) {
+          projectBackup.deleteAudioBackups.onComplete({
+            case Success(r) =>
+              println(s"Attempted to delete ${r.successCount} backups")
               System.exit(0)
             case Failure(exception) =>
               println(s"ERROR - Could not complete scan: ${exception.getMessage}")
