f394ca6c3e38fa73c5edfbbaaac2154388bec5a9
Richard Pryce <rich_pryce@yahoo.co.uk>
2024-03-04 16:42:59 +0000

    Rp/remove deadcode (#63)
    * Update org.postgresql dependency -> 43.7.2
    
    * Run apt-get update in github action
    
    * Update jose -> 9.37.2
    
    * Update sentry -> 7.5.0
    
    * Remove unused scripts

--- ci-build/containerscripts/awstools.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl "https://bootstrap.pypa.io/get-pip.py" -o "/tmp/get-pip.py"
-chmod a+x /tmp/get-pip.py
-python /tmp/get-pip.py
-rm -f /tmp/get-pip.py
-
-pip install awscli
