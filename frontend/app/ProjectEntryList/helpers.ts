4c1487b0e263f9c7b2000382ef14f7698ed0fa8d
Dave Allison <david.allison@theguardian.com>
2024-05-24 15:38:45 +0100

    Use an access token then save the file

--- frontend/app/ProjectEntryList/helpers.ts
+++ frontend/app/ProjectEntryList/helpers.ts
@@ -4,6 +4,7 @@ import {
   SystemNotifcationKind,
   SystemNotification,
 } from "@guardian/pluto-headers";
+import { saveAs } from "file-saver";
 
 const API = "/api";
 const API_PROJECTS = `${API}/project`;
@@ -581,8 +582,41 @@ export const getMissingFiles = async (id: number): Promise<MissingFiles[]> => {
 };
 
 export const downloadProjectFile = async (id: number) => {
-  const url = `${deploymentRootPath}${API_PROJECTS}/${id}/fileDownload?access_token=${localStorage.getItem(
-    "pluto:access-token"
-  )}`;
-  window.open(url);
+  const url = `${deploymentRootPath}${API_PROJECTS}/${id}/fileDownload`;
+
+  const token = localStorage.getItem("pluto:access-token");
+  if (!token) {
+    console.log("No local access token, performing request without it");
+  }
+
+  const toAddTo = {};
+
+  const newInit = Object.assign({}, toAddTo, {
+    headers: Object.assign({}, null, {
+      Authorization: `Bearer ${token}`,
+    }),
+  });
+
+  let filename = "";
+
+  fetch(url, newInit)
+    .then((response) => {
+      // @ts-ignore
+      filename = response.headers
+        .get("Content-Disposition")
+        .split('filename="')[1]
+        .split('";')[0];
+
+      if (filename.substr(filename.length - 1)) {
+        filename = filename.slice(0, -1);
+      }
+
+      return response.blob();
+    })
+    .then((blob) => {
+      saveAs(blob, filename);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
